c2fc36872401a680e3a7e6dbb6ef4e2d623e62e4
tomggb <lana.tomi.gabriel@gmail.com>
2024-10-16 10:37:03 -0300

    Refactor start.sh to run the private script in the background

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ python manage.py wait_for_db
 python manage.py migrate
 python manage.py collectstatic --noinput
 python dreamphish/run_public.py &
-python dreamphish/run_private.py
+python dreamphish/run_private.py &
